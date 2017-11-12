f8e03caa9fe97b5147d2d8aec7e3fe4f0bf75c63
Zuzanita <32848573+Zuzanita@users.noreply.github.com>
2017-11-12 18:06:54 +0100

    Add files via upload

---
+++ hra.py
@@ -0,0 +1,4 @@
+
+from Piskvorky import tah_hrace, tah, vyhodnot, piskvorky1d
+
+piskvorky1d()
---
+++ sibenice.py
@@ -0,0 +1,12 @@
+from random import randrange
+slovo == komín or koule or opice
+delka_slova = len.slovo
+randrange(0,2):
+    if 0:
+        slovo = komín
+    elif 1:
+        slovo = koule
+    else:
+        slovo = opice
+for i in range(delka_slova):
+    print("_" end=" ")
