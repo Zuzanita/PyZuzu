c5d2aebe633711dde3f02544648c1db107734613
Zuzana Pokorná <zuzanapokorna@seznam.cz>
2017-11-13 13:37:43 +0100

    Posílám úpravu modulů dle materiálu + své Piškvorky

---
+++ DP_06_testovani/ai.py
@@ -0,0 +1,11 @@
+import random
+
+def tah(herni_pole, cislo_policka, symbol):
+    return herni_pole[:cislo_policka] + symbol + herni_pole[cislo_policka + 1:]
+
+def tah_pocitace(herni_pole):
+
+    while True:
+        cislo_policka = random.randrange(len(herni_pole))
+        if herni_pole[cislo_policka] == "-":
+            return tah(herni_pole, cislo_policka, "o")
